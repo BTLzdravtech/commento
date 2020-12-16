0c81f0766a3c7ee7f045b24c1c1998bb9b84fbc3
Tomas Hruska <siccak@gmail.com>
2020-12-16 20:29:35 +0100

    apply MR 162
    https://gitlab.com/commento/commento/-/merge_requests/162

--- api/markdown.go
+++ api/markdown.go
@@ -13,6 +13,9 @@ func markdownRendererCreate() error {
 	policy = bluemonday.UGCPolicy()
 	policy.AddTargetBlankToFullyQualifiedLinks(true)
 
+	policy.RequireParseableURLs(true)
+	policy.AllowURLSchemes("mailto", "http", "https")
+
 	extensions = 0
 	extensions |= blackfriday.EXTENSION_AUTOLINK
 	extensions |= blackfriday.EXTENSION_STRIKETHROUGH
@@ -20,7 +23,6 @@ func markdownRendererCreate() error {
 	htmlFlags := 0
 	htmlFlags |= blackfriday.HTML_SKIP_HTML
 	htmlFlags |= blackfriday.HTML_SKIP_IMAGES
-	htmlFlags |= blackfriday.HTML_SAFELINK
 	htmlFlags |= blackfriday.HTML_HREF_TARGET_BLANK
 
 	renderer = blackfriday.HtmlRenderer(htmlFlags, "", "")
